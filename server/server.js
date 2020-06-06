14282ce7ab1fbb764ec399d89be3c52cddb0eebc
Bren Yamaguchi <yamaguchibren@gmail.com>
2020-06-06 15:20:25 -0700

    DB setup complete

--- server/server.js
+++ server/server.js
@@ -1,11 +1,24 @@
 const path = require('path');
 const express = require('express');
+const db = require('./model/db.js')
 
 const app = express();
 
 const PORT = 3000;
 
 app.use(express.json());
+
+// ***** This test works to our DB! *******
+
+app.get('/api/users', (req, res) => {
+  const queryText = 'SELECT * FROM users'
+  db.query(queryText)
+    .then(data => {
+      res.json(data.rows)
+    }).catch(err => {
+      res.json(err)
+    })
+});
 
 app.get('/', (req, res) =>
   res.status(200).sendFile(path.resolve(__dirname, '../client/index.html'))
