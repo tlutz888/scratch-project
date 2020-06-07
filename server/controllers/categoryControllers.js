f46e2940831e1c90e94419c3c2d39fe0d37e6afe
KateChantha <kubkate@gmail.com>
2020-06-07 10:18:08 -0700

    Finished category.js categoryContoller.js

--- server/controllers/categoryControllers.js
+++ server/controllers/categoryControllers.js
@@ -1,8 +1,81 @@
 const express = require('express');
 const db =  require('../model/db');
 
-const getCategory = async (req, res, next) => {};
-const createCategoty = async (req, res, next) => {}
+const categoryControllers = {};
 
-exports.getCategory = getCategory;
-exports.createCategoty = createCategoty;
+// categoryControllers.getCategoryByUserId = (req, res, next) => {
+
+//     const userId = req.params.id;
+
+//     const queryText = 'SELECT * From "category" WHERE user_id = $1';
+//     const values = [req.params.id];       
+
+//     db.query(queryText, values, (err, response) => {
+        //     if (err) {
+        //         console.log('Error in query for getCategoryByUserId: ', err)
+        //     }
+        //     res.locals.category =reponse.row[0]
+        //     return next();
+        // });
+
+//         .then(data => { =================================
+//             res.locals.data = data.rows;
+//             return next();
+//         })
+//         .catch(err => next({
+//             log: 'Error in categoryControllers.getCategory',
+//             status: 400,
+//             message: err 
+//         }));==============================================
+// };
+
+// categoryControllers.addCategoryByUserId = (req, res, next) => {
+//     const { category_id, title } = req.body;
+//     const values = [category_id, title];
+
+//     const queryText = 'INSERT INTO category'
+//     db.query(queryText, values)
+//         .then(data => {
+//             console.log(`${title} has been add to the DB`)
+//         })
+//         .catch(err => next({
+//             log: 'Error in categoryControllers.addCategory',
+//             status: 400,
+//             message: err,
+//         }))
+// };
+
+categoryControllers.getCategory = (req, res, next) => {
+    const queryText = 'SELECT * From "category"';
+    db.query(queryText)
+        .then(data => {
+            res.locals.data = data.rows;
+            return next();
+        })
+        .catch(err => next({
+            log: 'Error in categoryControllers.getCategory',
+            status: 400,
+            message: err 
+        }));
+};
+
+categoryControllers.addCategory = (req, res, next) => {
+    const { category_id, title } = req.body;
+    const values = [category_id, title];
+
+    const queryText = 'INSERT INTO category(category_id, title) VALUES($1, $2)'
+    db.query(queryText, values)
+        .then(data => {
+            console.log(`${title} has been add to the DB`)
+            return next();
+        })
+        .catch(err => next({
+            log: 'Error in categoryControllers.addCategory',
+            status: 400,
+            message: err,
+        }))
+};
+
+
+
+module.exports = categoryControllers;
--- server/routes/category.js
+++ server/routes/category.js
@@ -5,9 +5,16 @@ const categoryController = require('../controllers/categoryControllers');
 const router = express.Router();
 
 
-// app.use('/api/:user/categories', )
-router.get('/', categoryController.getCategory);
-router.post('/', categoryController.createCategoty);
+// app.use('/api/categories') /:user
+router.get('/api/categories', categoryController.getCategory, (req,res) => {
+    res.status(200).json(res.locals.data);
+});
+
+router.post('/api/categories', categoryController.addCategory, (req,res) => {
+    res.status(201).json('new category is added');
+});
+// router.get('/:user', categoryController.getCategory);
+// router.post('/:user', categoryController.addCategoty);
 
 
 
